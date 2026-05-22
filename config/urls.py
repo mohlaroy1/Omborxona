5c2b47a96a0e846f2ba4cfc68f3ad5e573bcd7f6
mohlaroy <mganijonova861@gmail.com>
2026-05-22 09:46:03 +0500

    Third commit

--- config/urls.py
+++ config/urls.py
@@ -4,6 +4,6 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
-    path('/statistics/', include('stats.urls')),
-    path('/accounts/', include('users.urls')),
+    path('statistics/', include('stats.urls')),
+    path('accounts/', include('users.urls')),
 ]
